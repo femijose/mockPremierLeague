df864c2d14932f42587fd820935556cb87c69288
Femi Jose <user@users-MacBook-Pro.local>
2019-10-03 23:25:34 +0100

    Modify redis config to resolve prod issue

--- src/app.js
+++ src/app.js
@@ -6,19 +6,29 @@ const userRouter = require('./routers/user')
 const rateLimiter = require('./middleware/ratelimiter')
 const session = require('express-session')
 const redis = require('redis');
-const client = redis.createClient()
+const client = redis.createClient(process.env.REDIS_URL, {no_ready_check: true})
 const redisStore = require('connect-redis')(session);
 
 const app = express()
 
-app.use(session({
+/*app.use(session({
     secret: process.env.REDIS_SESSION_SECRET,
     name: process.env.REDIS_SESSION_NAME,
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }, 
     store: new redisStore({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, client: client, ttl: process.env.REDIS_TTL }),
-  }))
+  })) */
+  app.use(
+    session({
+      store: new redisStore({ client }),
+      secret: process.env.REDIS_SESSION_SECRET,
+      name: process.env.REDIS_SESSION_NAME,
+      resave: false,
+      saveUninitialized: true,
+    cookie: { secure: false }, 
+    })
+  )
 app.use(express.json())
 app.use(rateLimiter)
 app.use(teamRouter)
